873edef08423329d8da9da913ee21accc840705f
Clark Schneider <cschneider@verticalrelevance.com>
2022-05-11 17:48:15 +0000

    add lambda_parse_results_detemine_compliance logic - all must be compliant, else fail

--- supplementary_files/lambdas/parse_results_determine_compliance/lambda_function.py
+++ supplementary_files/lambdas/parse_results_determine_compliance/lambda_function.py
@@ -2,7 +2,13 @@ def lambda_handler(event,context):
     
     print(event)
     
-    all_codebuild_inputs_compliant = True # FIXME
+    all_codebuild_inputs = [i['GetResultsReportIsCompliantBoolean']['Payload']['EvalEngineLambdalith']['Evaluation']['IsCompliant'] for i in event['ForEachCodeBuildInput']]
+
+    print(all_codebuild_inputs)
+    
+    all_codebuild_inputs_compliant = not any(all_codebuild_inputs)
+    
+    print(f'all_codebuild_inputs_compliant:\n{all_codebuild_inputs_compliant}')
     
     return {
         "AllCodeBuildInputsCompliant": all_codebuild_inputs_compliant
