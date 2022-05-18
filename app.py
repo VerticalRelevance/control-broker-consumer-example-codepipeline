ed29496994702ff083d5488d50bfe12af46bb49d
Clark Schneider <cschneider@verticalrelevance.com>
2022-05-18 13:19:46 +0000

    remove grand to CB readers

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from stacks.iac_pipeline_stack import ControlBrokerCodepipelineExampleStack
 app = cdk.App()
 ControlBrokerCodepipelineExampleStack(app, "CBConsumerCodepipeline",
     env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-    control_broker_input_reader_arns=app.node.try_get_context("control-broker/input-reader-arns"),
     control_broker_apigw_url=app.node.try_get_context("control-broker/apigw-url"),
     pipeline_ownership_metadata=app.node.try_get_context("control-broker/pipeline-ownership-metadata"),
 )
