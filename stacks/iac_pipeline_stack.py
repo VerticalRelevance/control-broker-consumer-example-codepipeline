fb9e412d2479f873628f7b0ff1e23f67f401952c
Clark Schneider <cschneider@verticalrelevance.com>
2022-05-19 17:28:33 +0000

    parse_tfplan_to_cb_input

--- stacks/iac_pipeline_stack.py
+++ stacks/iac_pipeline_stack.py
@@ -124,7 +124,7 @@ class ControlBrokerCodepipelineExampleStack(Stack):
             self,
             "ParseCdkOutToCBInput",
             sources=[
-                aws_s3_deployment.Source.asset(f"./supplementary_files/synth_utils")
+                aws_s3_deployment.Source.asset(f"./supplementary_files/codebuild_utils/synth_utils")
             ],
             destination_bucket=self.bucket_synth_utils,
             retain_on_delete=False,
@@ -171,19 +171,6 @@ class ControlBrokerCodepipelineExampleStack(Stack):
             )
         )
 
-        synthed_templates_s3_uri_root = f"s3://{self.bucket_synthed_templates.bucket_name}/"
-
-        def s3_uri_to_bucket(*,Uri):
-            path_parts=Uri.replace("s3://","").split("/")
-            bucket=path_parts.pop(0)
-            return bucket
-            
-        def s3_uri_to_key(*,Uri):
-            path_parts=Uri.replace("s3://","").split("/")
-            bucket=path_parts.pop(0)
-            key="/".join(path_parts)
-            return key
-        
         self.codebuild_to_sfn_artifact_file = "control-broker-consumer-inputs.json"
             
         build_project_cdk_synth = aws_codebuild.PipelineProject(
@@ -249,7 +236,7 @@ class ControlBrokerCodepipelineExampleStack(Stack):
 
         self.bucket_tfplan = aws_s3.Bucket(
             self,
-            "SynthedTemplates",
+            "TFPlan",
             block_public_access=aws_s3.BlockPublicAccess.BLOCK_ALL,
             removal_policy=RemovalPolicy.DESTROY,
             auto_delete_objects=True,
@@ -257,15 +244,15 @@ class ControlBrokerCodepipelineExampleStack(Stack):
 
         CfnOutput(
             self,
-            "SynthedTemplatesBucket",
+            "TFPlanBucket",
             value=self.bucket_tfplan.bucket_name,
         )
 
-        # synth
+        # tfplan
         
-        self.bucket_synth_utils = aws_s3.Bucket(
+        self.bucket_tfplan_utils = aws_s3.Bucket(
             self,
-            "SynthUtils",
+            "TFPlanUtils",
             block_public_access=aws_s3.BlockPublicAccess.BLOCK_ALL,
             removal_policy=RemovalPolicy.DESTROY,
             auto_delete_objects=True,
@@ -273,21 +260,21 @@ class ControlBrokerCodepipelineExampleStack(Stack):
 
         aws_s3_deployment.BucketDeployment(
             self,
-            "ParseCdkOutToCBInput",
+            "ParseTFPlanOutputToCBInput",
             sources=[
-                aws_s3_deployment.Source.asset(f"./supplementary_files/synth_utils")
+                aws_s3_deployment.Source.asset(f"./supplementary_files/codebuild_utils/tfplan_utils")
             ],
-            destination_bucket=self.bucket_synth_utils,
+            destination_bucket=self.bucket_tfplan_utils,
             retain_on_delete=False,
         )
         
-        role_synth = aws_iam.Role(
+        role_tfplan = aws_iam.Role(
             self,
-            "Synth",
+            "TFPlan",
             assumed_by=aws_iam.ServicePrincipal("codebuild.amazonaws.com"),
         )
 
-        role_synth.add_to_policy(
+        role_tfplan.add_to_policy(
             aws_iam.PolicyStatement(
                 actions=[
                     "s3:PutObject",
@@ -300,7 +287,7 @@ class ControlBrokerCodepipelineExampleStack(Stack):
                 ],
             )
         )
-        role_synth.add_to_policy(
+        role_tfplan.add_to_policy(
             aws_iam.PolicyStatement(
                 actions=[
                     "s3:GetObject",
@@ -308,12 +295,12 @@ class ControlBrokerCodepipelineExampleStack(Stack):
                     "s3:List*",
                 ],
                 resources=[
-                    self.bucket_synth_utils.bucket_arn,
-                    self.bucket_synth_utils.arn_for_objects("*"),
+                    self.bucket_tfplan_utils.bucket_arn,
+                    self.bucket_tfplan_utils.arn_for_objects("*"),
                 ],
             )
         )
-        role_synth.add_to_policy(
+        role_tfplan.add_to_policy(
             aws_iam.PolicyStatement(
                 actions=[
                     "codepipeline:GetPipelineState",
@@ -322,25 +309,12 @@ class ControlBrokerCodepipelineExampleStack(Stack):
             )
         )
 
-        tfplan_s3_uri_root = f"s3://{self.bucket_tfplan.bucket_name}/"
-
-        def s3_uri_to_bucket(*,Uri):
-            path_parts=Uri.replace("s3://","").split("/")
-            bucket=path_parts.pop(0)
-            return bucket
-            
-        def s3_uri_to_key(*,Uri):
-            path_parts=Uri.replace("s3://","").split("/")
-            bucket=path_parts.pop(0)
-            key="/".join(path_parts)
-            return key
-        
         self.codebuild_to_sfn_artifact_file = "control-broker-consumer-inputs.json"
             
-        build_project_cdk_synth = aws_codebuild.PipelineProject(
+        build_project_tfplan = aws_codebuild.PipelineProject(
             self,
-            "CdkSynth",
-            role = role_synth,
+            "TFPlan",
+            role = role_tfplan,
             build_spec=aws_codebuild.BuildSpec.from_object(
                 {
                     "version": "0.2",
@@ -348,12 +322,11 @@ class ControlBrokerCodepipelineExampleStack(Stack):
                         "install": {
                             "on-failure": "ABORT",
                             "commands": [
-                                # TODO upgrade node, v10 deprecated
-                                "npm install -g typescript",
-                                "npm install -g ts-node",
-                                "npm install -g aws-cdk",
-                                "npm install",
-                                "cdk --version",
+                                "sudo yum install -y yum-utils",
+                                "sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo",
+                                "sudo yum -y install terraform",
+                                "terraform -install-autocomplete",
+                                "terraform -version",
                             ],
                         },
                         "build": {
@@ -362,10 +335,9 @@ class ControlBrokerCodepipelineExampleStack(Stack):
                                 "CODEPIPELINE_EXECUTION_ID=$(aws codepipeline get-pipeline-state --region us-east-1 --name ${CODEBUILD_INITIATOR#codepipeline/} --query 'stageStates[?actionStates[?latestExecution.externalExecutionId==`'${CODEBUILD_BUILD_ID}'`]].latestExecution.pipelineExecutionId' --output text)",
                                 "echo $CODEPIPELINE_EXECUTION_ID",
                                 "ls",
-                                "cdk synth",
+                                "cdk terraform plan --out tfplan.binary && terraform show -json tfplan.binary > tfplan.json",
                                 "ls",
-                                "ls cdk.out",
-                                f"aws s3 sync s3://{self.bucket_synth_utils.bucket_name} .",
+                                f"aws s3 sync s3://{self.bucket_tfplan_utils.bucket_name} .",
                                 "pip install -r requirements.txt",
                                 f"python3 parse_cdk_out_to_cb_input.py {self.codebuild_to_sfn_artifact_file} $CODEPIPELINE_EXECUTION_ID",
                             ],
@@ -379,19 +351,19 @@ class ControlBrokerCodepipelineExampleStack(Stack):
                 }
             ),
             environment_variables={
-                "SynthedTemplatesBucket": aws_codebuild.BuildEnvironmentVariable(value=self.bucket_tfplan.bucket_name),
+                "TFPlanBucket": aws_codebuild.BuildEnvironmentVariable(value=self.bucket_tfplan.bucket_name),
                 "PipelineOwnershipMetadata": aws_codebuild.BuildEnvironmentVariable(value=json.dumps(self.pipeline_ownership_metadata)),
             }
             
         )
 
-        self.artifact_synthed = aws_codepipeline.Artifact()
+        self.artifact_tfplan = aws_codepipeline.Artifact()
 
         self.action_build = aws_codepipeline_actions.CodeBuildAction(
             action_name="CodeBuildCdkSynth",
-            project=build_project_cdk_synth,
+            project=build_project_tfplan,
             input=self.artifact_source,
-            outputs=[self.artifact_synthed],
+            outputs=[self.artifact_tfplan],
         )
     
     def evaluate_wrapper_sfn_lambdas(self):
@@ -416,21 +388,37 @@ class ControlBrokerCodepipelineExampleStack(Stack):
                 self.layers['aws_requests_auth'],
             ]
         )
-        self.lambda_sign_apigw_request.role.add_to_policy(
-            aws_iam.PolicyStatement(
-                actions=[
-                    "s3:GetObject",
-                    "s3:GetBucket",
-                    "s3:List*",
-                ],
-                resources=[
-                    self.bucket_synthed_templates.bucket_arn,
-                    self.bucket_synthed_templates.arn_for_objects("*"),
-                    self.bucket_tfplan.bucket_arn,
-                    self.bucket_tfplan.arn_for_objects("*"),
-                ],
+
+        if self.source_iac == "CDK":
+            
+            self.lambda_sign_apigw_request.role.add_to_policy(
+                aws_iam.PolicyStatement(
+                    actions=[
+                        "s3:GetObject",
+                        "s3:GetBucket",
+                        "s3:List*",
+                    ],
+                    resources=[
+                        self.bucket_synthed_templates.bucket_arn,
+                        self.bucket_synthed_templates.arn_for_objects("*"),
+                    ],
+                )
             )
-        )
+        if self.source_iac == "Terraform":
+
+            self.lambda_sign_apigw_request.role.add_to_policy(
+                aws_iam.PolicyStatement(
+                    actions=[
+                        "s3:GetObject",
+                        "s3:GetBucket",
+                        "s3:List*",
+                    ],
+                    resources=[
+                        self.bucket_tfplan.bucket_arn,
+                        self.bucket_tfplan.arn_for_objects("*"),
+                    ],
+                )
+            )
         
         #requests get
         
